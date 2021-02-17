289ee42e8b1b22e1effd6f19f24337ba280c0837
Christopher Ortega <A01634500@itesm.mx>
2021-02-16 20:18:06 -0700

    Solve Activity#3 Semana2

--- ajax01.go
+++ ajax01.go
@@ -1,27 +1,24 @@
 package main
 
 import (
-  "fmt"
-  
-  "net/http"
-
+	"fmt"
+	"net/http"
 )
 
-
-
 func darMensaje(w http.ResponseWriter, r *http.Request) {
-  
-  
-  fmt.Printf("%s", r)
+	data := [5]int{666, 420, 69, 1313, 42069}
 
-
+	fmt.Fprintf(w, "data: %v", data)
+	//http.ServeFile(w, r, "data")
 
 }
 
 func main() {
-  http.HandleFunc("/", darMensaje)
-  err := http.ListenAndServe("localhost"+":"+"8080", nil)
-  if err != nil {
-    return
-  }
+
+	http.HandleFunc("/", darMensaje)
+	err := http.ListenAndServe("localhost"+":"+"8080", nil)
+	fmt.Println("Server READY!!")
+	if err != nil {
+		return
+	}
 }
