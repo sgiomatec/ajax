2188f83e905175e9c1b0348c7d04669d0672843b
Christopher Ortega <A01634500@itesm.mx>
2021-02-28 12:51:34 -0700

    Finished Reto 3

--- ajax01.go
+++ ajax01.go
@@ -1,30 +1,25 @@
 package main
 
 import (
-  "fmt"
-  
-  "net/http"
+	"fmt"
 
+	"net/http"
 )
 
-
-
-func darMensaje(w http.ResponseWriter, r *http.Request) {
-  fmt.Printf("%s\n", r.Referer())
-  //fmt.Printf("%s\n", r.URL)
-  //fmt.Printf("%s", r.Method)
-  //fmt.Printf("%s", r.UserAgent())
-  //fmt.Fprintf(w, "something at the array: %d! \n'", data[4])
-  //http.ServeFile(w, r, "ajax02.html")
-
-
+func darMsj(w http.ResponseWriter, r *http.Request) {
+	fmt.Printf("%s\n", r.Referer())
+	//fmt.Printf("%s\n", r.URL)
+	//fmt.Printf("%s", r.Method)
+	//fmt.Printf("%s", r.UserAgent())
+	//fmt.Fprintf(w, "something at the array: %d! \n'", data[4])
+	//http.ServeFile(w, r, "ajax02.html")
 
 }
 
 func main() {
-  http.HandleFunc("/", darMensaje)
-  err := http.ListenAndServe("localhost"+":"+"8080", nil)
-  if err != nil {
-    return
-  }
+	http.HandleFunc("/", darMsj)
+	err := http.ListenAndServe("localhost"+":"+"8080", nil)
+	if err != nil {
+		return
+	}
 }
--- ajax02.go
+++ ajax02.go
@@ -1,119 +1,204 @@
 package main
 
 import (
- "fmt"
-  "net/http"
-  "encoding/json"
-  "strconv"
-
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"strconv"
+	"time"
 )
-
 
 type book struct {
 	Titulo string `json: titulo`
-	Autor  string    `json: autor`
+	Autor  string `json: autor`
 }
-type data struct {
-	data string `json: data`
+type UserData struct {
+	User    string
+	Mail    string
+	Lastcon string
+}
 
+type SendUser struct {
+	User    bool
+	Mail    bool
+	Lastcon bool
 }
+
+var data = [4]UserData{
+	UserData{"Nick", "nicks@anymail.com", "02-28-2021 09:38:22"},
+	UserData{"Luis", "luis.eds@anymail.com", "02-27-2021 19:12:02"},
+	UserData{"Chris", "chris@anymail.com", "02-25-2021 12:08:12"},
+	UserData{"Oscar", "the_odb@anymail.com", "02-20-2021 17:58:09"},
+}
+
+var changed = [4]SendUser{
+	SendUser{false, false, false},
+	SendUser{false, false, false},
+	SendUser{false, false, false},
+	SendUser{false, false, false},
+}
+
+// var reset = [4][3]bool{{false, false, false}, {false, false, false}, {false, false, false}, {false, false, false}}
 
 func mostrarHTML(w http.ResponseWriter, r *http.Request) {
-  
-   http.ServeFile(w, r, "ajax02.html")
+
+	http.ServeFile(w, r, "ajax02.html")
 }
 func cliente(w http.ResponseWriter, r *http.Request) {
-  
-  http.ServeFile(w, r, "cliente1.html")
+
+	http.ServeFile(w, r, "cliente1.html")
 }
 func cliente2(w http.ResponseWriter, r *http.Request) {
-  
-  http.ServeFile(w, r, "cliente2.html")
+
+	http.ServeFile(w, r, "cliente2.html")
+}
+
+func cliente3(w http.ResponseWriter, r *http.Request) {
+
+	http.ServeFile(w, r, "cliente3.html")
 }
 
 func darMensaje(w http.ResponseWriter, r *http.Request) {
 
-	r.ParseForm() 
+	r.ParseForm()
 	x := r.Form.Get("y")
-  
-  	fmt.Printf("%s", x)
 
-  
+	fmt.Printf("%s", x)
 
-  	libro := book{
-  			Titulo: "La Casa",
-  			Autor:  "Paco Roca",
-  		}
+	libro := book{
+		Titulo: "La Casa",
+		Autor:  "Paco Roca",
+	}
 
-   
-  //w.Header().Set("Content-Type", "application/json") 
+	//w.Header().Set("Content-Type", "application/json")
 
-     json.NewEncoder(w).Encode(libro)
+	json.NewEncoder(w).Encode(libro)
 
 }
 func reto1(w http.ResponseWriter, r *http.Request) {
- 
-  arreglo := [6]string{"hola","amigo","Como","estas","adelante", "pasaa"}
-  
 
-	r.ParseForm() 
-  w.Header().Set("Content-Type", "application/json") 
-  if r.Method == "GET"{
-    fmt.Fprintf(w,arreglo[5])
-  } else if r.Method == "POST"{
-    pos := r.Form.Get("y")
-    dat := r.Form.Get("n")
-    i , _:=strconv.Atoi(pos)
+	arreglo := [6]string{"hola", "amigo", "Como", "estas", "adelante", "pasaa"}
 
-    arreglo[i%5]=dat
-    fmt.Fprintf(w,"ok")
+	r.ParseForm()
+	w.Header().Set("Content-Type", "application/json")
+	if r.Method == "GET" {
+		fmt.Fprintf(w, arreglo[5])
+	} else if r.Method == "POST" {
+		pos := r.Form.Get("y")
+		dat := r.Form.Get("n")
+		i, _ := strconv.Atoi(pos)
 
-  }
-	
+		arreglo[i%5] = dat
+		fmt.Fprintf(w, "ok")
+
+	}
+
 }
+
 func reto2(w http.ResponseWriter, r *http.Request) {
- 
-  arreglo := [4]string{"{\"Persona\":\"Nick\",\"Libro\":\"Orgullo y Prejuicio\",\"mascota\":\"manchas\"}",
-                       "{\"Persona\":\"Luis\",\"Libro\":\"Harry Potter\",\"mascota\":\"Sissi\"}",
-                       "{\"Persona\":\"Chris\",\"Libro\":\"Quiubole con\",\"mascota\":\"Nina\"}",
-                       "{\"Persona\":\"Oscar\",\"Libro\":\"Sapiens\",\"mascota\":\"Pirata\"}"}
-  
+	arreglo := [4]string{
+		"{\"Persona\":\"Nick\",\"Libro\":\"Orgullo y Prejuicio\",\"mascota\":\"manchas\"}",
+		"{\"Persona\":\"Luis\",\"Libro\":\"Harry Potter\",\"mascota\":\"Sissi\"}",
+		"{\"Persona\":\"Chris\",\"Libro\":\"Quiubole con\",\"mascota\":\"Nina\"}",
+		"{\"Persona\":\"Oscar\",\"Libro\":\"Sapiens\",\"mascota\":\"Pirata\"}",
+	}
 
-	r.ParseForm() 
-  w.Header().Set("Content-Type", "application/json") 
-  
-    /* index := r.Form.Get("y")
-    fmt.Printf("Position: %s\n",index);
-    i , _:=strconv.Atoi(index)
-    fmt.Printf("value: %d\n",i);
+	r.ParseForm()
+	w.Header().Set("Content-Type", "application/json")
 
-    fmt.Fprintf(w,arreglo[i%4]) */
-    if r.Method == "POST"{
-
-      pos := r.Form.Get("y")
-    
-      i , _:=strconv.Atoi(pos)
-      fmt.Printf("%s\n",pos)
-      fmt.Fprintf(w,arreglo[i%4])
-  
-    }
-  
-	
+	if r.Method == "POST" {
+		pos := r.Form.Get("y")
+		i, _ := strconv.Atoi(pos)
+		fmt.Printf("%s\n", pos)
+		fmt.Fprintf(w, arreglo[i%4])
+	}
 }
 
+func reto3(w http.ResponseWriter, r *http.Request) {
+	if r.Method == "GET" {
+		toSend := [4]UserData{}
+		for i, v := range changed {
 
+			fmt.Println(i, v)
+			// checking for changes in data
+			var n, m, d string
+
+			if !v.User {
+				fmt.Println(v.User, "YES sending user...")
+				n = data[i].User
+				v.User = true
+			} else if v.User {
+				fmt.Println(v.User, "NOT sending user...")
+				n = ""
+			}
+
+			if !v.Mail {
+				m = data[i].Mail
+				v.Mail = true
+			} else if v.Mail {
+				m = ""
+			}
+
+			if !v.Lastcon {
+				d = data[i].Lastcon
+				v.Lastcon = true
+			} else if v.Lastcon {
+				d = ""
+			}
+
+			fmt.Println(i, v)
+			fmt.Println("===================")
+
+			changed[i] = v
+			toSend[i] = UserData{n, m, d}
+
+		}
+		json.NewEncoder(w).Encode(toSend)
+	} else if r.Method == "POST" {
+		w.Header().Set("Content-Type", "application/json")
+		r.ParseForm()
+		id, _ := strconv.Atoi(r.Form.Get("id"))
+		name := r.Form.Get("name")
+		mail := r.Form.Get("mail")
+		date, _ := strconv.ParseBool(r.Form.Get("date"))
+
+		fmt.Println("Updating data...")
+
+		if name != "" {
+			fmt.Println("name: " + data[id].User + "->" + name)
+			data[id].User = name
+			changed[id].User = false
+		}
+		if mail != "" {
+			fmt.Println("mail: " + data[id].Mail + "->" + mail)
+			data[id].Mail = mail
+			changed[id].Mail = false
+		}
+		if date {
+			newDate := time.Now().Format("02-01-2006 15:04:05")
+			fmt.Println("date: " + data[id].Lastcon + "->" + newDate)
+			data[id].Lastcon = newDate
+			changed[id].Lastcon = false
+		}
+
+		fmt.Println(changed[id])
+	}
+
+}
 
 func main() {
-	
-  http.HandleFunc("/dato", darMensaje)
-  http.HandleFunc("/",mostrarHTML)
-  http.HandleFunc("/reto1",reto1)
-  http.HandleFunc("/reto2",reto2)
-  http.HandleFunc("/cliente",cliente)
-  http.HandleFunc("/cliente2",cliente2)
-  
-  err := http.ListenAndServe("localhost"+":"+"8080", nil)
-  if err != nil {
-    return
-  }
+
+	http.HandleFunc("/dato", darMensaje)
+	http.HandleFunc("/", mostrarHTML)
+	http.HandleFunc("/reto1", reto1)
+	http.HandleFunc("/reto2", reto2)
+	http.HandleFunc("/reto3", reto3)
+	http.HandleFunc("/cliente", cliente)
+	http.HandleFunc("/cliente2", cliente2)
+	http.HandleFunc("/cliente3", cliente3)
+	fmt.Println(time.Now().Format("02-01-2006 15:04:05"))
+	err := http.ListenAndServe("localhost"+":"+"8080", nil)
+	if err != nil {
+		return
+	}
 }
